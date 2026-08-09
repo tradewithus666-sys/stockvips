8d34c653bf4047ac96dde0526867400fe479bd9f
tradewithus666-sys <tradewithus666@gmail.com>
2026-08-09 09:00:37 +0800

    Fix missing fetchActiveAnnouncements import

--- src/pages/MemberCenter.jsx
+++ src/pages/MemberCenter.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { fetchMyPermissions, fetchMyPurchases, fetchArticlesByProduct, purchaseWithBalance, createPaymentIntent, toggleEmailNotify } from '../lib/api';
+import { fetchMyPermissions, fetchMyPurchases, fetchArticlesByProduct, purchaseWithBalance, fetchActiveAnnouncements, createPaymentIntent, toggleEmailNotify } from '../lib/api';
 import { useAuth } from '../lib/AuthContext';
 import { useToast } from '../lib/ToastContext';
 import { useLang } from '../lib/LangContext';
